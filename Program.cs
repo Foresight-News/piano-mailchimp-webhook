4d2da3562c3cd501a9d59aa3af5a8f3850cc362e
John Tangen <john@trekanin.com>
2026-04-19 23:51:55 +0100

    Implement newsletter preference mapping service
    Provides a mapper to translate Piano custom fields into Mailchimp interest categories based on configuration. It includes flexible type conversion for various Piano field values and a utility to determine if specific user updates affect managed newsletter fields.

--- Program.cs
+++ Program.cs
@@ -6,12 +6,15 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.Configure<MailchimpOptions>(
     builder.Configuration.GetSection(MailchimpOptions.SectionName));
+builder.Services.Configure<NewsletterMappingOptions>(
+    builder.Configuration.GetSection(NewsletterMappingOptions.SectionName));
 builder.Services.Configure<PianoOptions>(
     builder.Configuration.GetSection(PianoOptions.SectionName));
 builder.Services.Configure<PianoWebhookOptions>(
     builder.Configuration.GetSection(PianoWebhookOptions.SectionName));
 builder.Services.AddHttpClient<IMailchimpAudienceService, MailchimpAudienceService>();
 builder.Services.AddHttpClient<IPianoApiClient, PianoApiClient>();
+builder.Services.AddSingleton<INewsletterPreferenceMapper, NewsletterPreferenceMapper>();
 builder.Services.AddScoped<IPianoWebhookService, PianoWebhookService>();
 
 var app = builder.Build();
